8753b6f924f8576f72239d2488988eea84792a46
jm33-m0 <jm33@jm33.me>
2022-01-29 09:51:19 +0800

    cleanup

--- run.go
+++ run.go
@@ -3,9 +3,8 @@ package golpe
 import "log"
 
 var All = map[string]func() error{
-	"CVE-2021-4034": CVE_2021_4034, // pkexec
-	// "CVE-2021-4034": blasty_vs_pkexec, // pkexec
-	// "CVE-2018-14655": CVE_2018_14655, // xorg
+	"CVE-2021-4034":  CVE_2021_4034,  // pkexec
+	"CVE-2018-14655": CVE_2018_14655, // xorg
 }
 
 func RunAll() (err error) {
