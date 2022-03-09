63cede19a790959b383323bba8e3cfca0e2064f4
jm33-m0 <jm33@jm33.me>
2022-03-09 13:56:57 +0800

    convert binary file to (bz2 compressed) base64 text

---
+++ cmd/elf2str/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	golpe "github.com/jm33-m0/go-lpe"
+)
+
+func main() {
+	elf_path := flag.String("file", "", "target file to encode")
+	flag.Parse()
+
+	elf_data, err := ioutil.ReadFile(*elf_path)
+	if err != nil {
+		log.Fatalf("read %s: %v", *elf_path, err)
+	}
+
+	elf_str := golpe.Bin2String(elf_data)
+
+	// fmt.Printf("\n%s\n\n", elf_str)
+	fmt.Printf("%s\n", format_elf_str(elf_str))
+}
+
+func format_elf_str(str string) (ret string) {
+	linelen := 50
+	if len(str) < linelen {
+		return str
+	}
+
+	// initialize
+	ret = fmt.Sprintf("const elf_str = `%s`", str[:linelen])
+
+	line := ""
+	for n, c := range str {
+		line += string(c)
+		if n%linelen == 0 {
+			ret = fmt.Sprintf("%s +\n`%s`", ret, line)
+			line = ""
+		}
+	}
+
+	return
+}
--- util.go
+++ util.go
@@ -9,6 +9,11 @@ import (
 	"github.com/mholt/archiver/v3"
 )
 
+// Base64Encode decode a base64 encoded string (to []byte)
+func Base64Encode(data []byte) string {
+	return base64.URLEncoding.EncodeToString(data)
+}
+
 // Base64Decode decode a base64 encoded string (to []byte)
 func Base64Decode(text string) []byte {
 	dec, err := base64.URLEncoding.DecodeString(text)
@@ -17,6 +22,27 @@ func Base64Decode(text string) []byte {
 		return nil
 	}
 	return dec
+}
+
+// Bin2String compress binary file and encode with base64
+func Bin2String(data []byte) (res string) {
+	bz2 := &archiver.Bz2{}
+	var compressed_buf bytes.Buffer
+	reader := bytes.NewReader(data)
+	err := bz2.Compress(reader, &compressed_buf)
+	if err != nil {
+		log.Printf("Bin2String: %v", err)
+		return
+	}
+	compressed_bin := compressed_buf.Bytes()
+
+	// encode
+	res = Base64Encode(compressed_bin)
+	if res == "" {
+		log.Println("Bin2String failed, empty string generated")
+	}
+
+	return
 }
 
 // ExtractFileFromString base64 decode, and then decompress using bzip2
